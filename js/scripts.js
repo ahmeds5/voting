c8f4a2ce3dcc0f26f391744e2476cceb1ae291fa
Lawrence Eby and said ahmed <said@>
2017-01-12 09:04:20 -0800

    add jQuery functionality to our existing html. Also add links.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $("#blank form").submit(function(event) {
+    var inputedAge = parseInt($("input#age").val());
+
+    if (inputedAge >= 18) {
+      $(".minor").hide();
+      $(".adult").slideDown();
+    } else {
+      $(".adult").hide();
+      $(".minor").slideDown();
+    }
+    event.preventDefault();
+  })
 });
